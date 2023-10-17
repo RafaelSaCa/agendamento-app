c9e78f3b012d88ef5812b0684b3f8a4cc81cbe11
Rafael Saca <raffaellsaca@gmail.com>
2023-10-16 22:47:25 -0300

    feat: table paciente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
--- src/app/pacientes/pacientes.module.ts
+++ src/app/pacientes/pacientes.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
 import { PacientesRoutingModule } from './pacientes-routing.module';
 import { PacientesComponent } from './pacientes/pacientes.component';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,11 @@ import { PacientesComponent } from './pacientes/pacientes.component';
   ],
   imports: [
     CommonModule,
-    PacientesRoutingModule
+    PacientesRoutingModule,
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule
+
   ]
 })
 export class PacientesModule { }
